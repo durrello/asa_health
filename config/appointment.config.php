c97062ed4962cdc21f8323af9f13fc24500dfa27
Durrell Gemuh <durrellgemuh07@gmail.com>
2020-10-24 08:42:41 +0100

    improved on the backend security of the forms

--- config/appointment.config.php
+++ config/appointment.config.php
@@ -2,11 +2,11 @@
 require_once "db.config.php";
 $fname = $lname = $country = $city = $email = $phone = $subject = $message ='';
 
-$fname = $_POST['fname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$fname = mysqli_real_escape_string($conn, $_POST['fname']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$phone = mysqli_real_escape_string($conn, $_POST['phone']);
+$subject = mysqli_real_escape_string($conn, $_POST['subject']);
+$message = mysqli_real_escape_string($conn, $_POST['message']);
 
 // insert into table appointments
 $sql = "INSERT INTO appointment (fname, email, phone, subject, message ) VALUES ('$fname', '$email', '$phone', '$subject', '$message')";
--- config/contact.config.php
+++ config/contact.config.php
@@ -2,10 +2,10 @@
 require_once "db.config.php";
 $fname = $lname = $country = $city = $email = $phone = $subject = $message ='';
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$subject = mysqli_real_escape_string($conn, $_POST['subject']);
+$message = mysqli_real_escape_string($conn, $_POST['message']);
 
 // insert into table appointments
 $sql = "INSERT INTO contact (name, email, subject, message ) VALUES ('$name', '$email', '$subject', '$message')";
